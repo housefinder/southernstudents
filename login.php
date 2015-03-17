163b687f4da70eb7fa6b29ae9ede7025ad7e0a17
Luke <lukepelling1@gmail.com>
2015-03-17 16:51:34 +0000

    Create login.php

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+    require_once("../includes/session.php");
+    require_once("../includes/connect.php");
+    require_once("../includes/functions.php");
+
+    if(isset($_POST["login"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $query = "SELECT * FROM properties_users WHERE username='{$username}' AND password='{$password}' LIMIT 1";
+
+        $result = mysqli_query($connection, $query);
+
+        if($user = mysqli_fetch_assoc($result)) {
+            $_SESSION["message"] = "Success, welcome back {$user["username"]}";
+            $_SESSION["username"] = $user["username"];
+            $_SESSION["user_id"] = $user["id"];
+            $_SESSION["user_level"] = 5;
+
+        } else {
+            $_SESSION["message"] = "Wrong username/password";
+        }
+
+    }
+
+    redirectTo("index.php");
+
+?>
