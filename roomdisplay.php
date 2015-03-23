291bd6902195fb7228cdf450f44f9fa883fab73e
Luke <lukepelling1@gmail.com>
2015-03-23 16:56:04 +0000

    Create roomdisplay.php

---
+++ roomdisplay.php
@@ -0,0 +1,15 @@
+<html>
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+
+<div id="roomdisplay">
+    
+    <p> <!--Title displayed here
+        <br> Description displayed here
+        <br> Postcode displayed here
+        <br> Bedrooms displayed here
+        <br> Tenants gender displayed here
+        <br> University displayed here-->
+    </p> 
+    
+</div>
+</html>
