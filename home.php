c56b09ce0904f160b10dfccb6682dc031dbe9a82
Luke <lukepelling1@gmail.com>
2015-03-23 16:55:11 +0000

    Create home.php

---
+++ home.php
@@ -0,0 +1,23 @@
+<?php include_once("../includes/templates/header.php"); ?>
+
+<html>
+
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+
+<body class="news">
+  
+<div id="main"> 
+    <p> Welcome to StudentHousemateFinder, let's get started <br>
+    <br> <button type="button" class="homeButton">Let Me Search For A Room</button>
+         <button type="button" class="homeButton">I Want To Post A Room</button>
+    </p>
+</div>
+            
+<div id="para2">
+    <p>
+        <img src="images/houseLogo.jpg">
+        
+    </p>
+</div>
+              
+ <?php include_once("../includes/templates/footer.php"); ?>
