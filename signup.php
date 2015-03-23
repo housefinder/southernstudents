64fd128ffff5fd6fce26c28c23e057f65b9ade7a
Luke <lukepelling1@gmail.com>
2015-03-23 16:53:06 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -13,11 +13,49 @@
         $lastname = $_POST["lastname"];
         $email = $_POST["email"];
         $university = $_POST["university"];
+    } else {
+        $username = "";
+        $password = "";
+        $firstname = "";
+        $lastname = "";
+        $email = "";
+        $university = "";
+    } 
+?>
 
+<?php
+    if(isset($_POST["submit"])) {
+        
+        if(empty($username)) {
+            $mess = "Please enter username";
+        } else if(empty($password)) {
+            $mess = "Please enter password";
+        } else if(empty($firstname)) {
+            $mess = "Please enter firstname";
+        } else if(empty($lastname)) {
+            $mess = "Please enter lastname";
+        } else if(empty($email)) {
+            $mess = "Please enter email";
+        } else if(empty($university)) {
+            $mess = "Please enter university";
+        } else { 
         
         $query = "INSERT INTO users (username, password, firstname, lastname, email, university) VALUES ('{$username}', '{$password}', '{$firstname}', '{$lastname}', '{$email}', '{$university}')";
-            
         mysqli_query($connection, $query);
+            
+            if($result) {
+                $mess = "Success";
+            } else {
+                $mess = "Sorry";
+            }
+        $username = "";
+        $password = "";
+        $firstname = "";
+        $lastname = "";
+        $email = "";
+        $university = "";
+            
+        }
         
     }
 
@@ -34,14 +72,12 @@
     <div id="para2">
     <p>
         
+        <?php if(isset($mess)) { 
+         echo $mess; } ?>
+        
       <?php include_once("../includes/templates/signupform.php"); ?>
        
     </p>
     </div>   
-           
-  
- 		<div id="footer">
-			Copyright Housemate Finder
-		</div>
-  
+             
 <?php include_once("../includes/templates/footer.php"); ?> 
