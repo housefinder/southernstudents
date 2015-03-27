bfb536653b750e8d24d4188b198de278c4c15746
Luke <lukepelling1@gmail.com>
2015-03-27 15:12:31 +0000

    Create search.php

---
+++ search.php
@@ -0,0 +1,55 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?>
+    
+<?php
+
+    if(isset($_POST["submit"])) {
+        
+        $gender = $_POST["gender"];
+        $address = $_POST["address"];
+        $university = $_POST["university"];
+        $postcode = $_POST["postcode"];
+        $description = $_POST["description"];
+        $bedrooms = $_POST["bedrooms"];
+        $rent = $_POST["rent"];
+        $contact = $_POST["contact"];
+        
+        $query = "INSERT INTO house (gender, address, university, postcode, description, bedrooms, rent, contact) VALUES ('{$gender}', '{$address}', '{$university}', '{$postcode}', '{$description}', '{$bedrooms}', '{$rent}', '{$contact}')";
+
+        mysqli_query($connection, $query);
+        
+    }
+?>
+
+<?php
+    if(isset($_POST["university"])){
+        $query = "SELECT * FROM house WHERE university = '$_POST[university]'";
+    }
+?>
+
+<html>
+
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+   
+<?php require_once("../includes/templates/submitform.php"); ?>
+    
+<?php require_once("../includes/templates/filterform.php"); ?>
+    
+<div class="results">
+<?php
+	$query = "SELECT * FROM house";
+	$result = mysqli_query($connection, $query);
+	if(!$result) {
+		die("Database query failed.");
+	}
+
+	 while($row = mysqli_fetch_assoc($result)) {
+	 include("../includes/templates/roomdisplay.php");
+	}
+
+?>
+</div>
+ 
+<?php include_once("../includes/templates/footer.php"); ?>
