9e471dc244cc09a3f14cf6487ad1836fe670919c
Luke <lukepelling1@gmail.com>
2015-04-06 16:12:37 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -27,17 +27,17 @@
     if(isset($_POST["submit"])) {
         
         if(empty($username)) {
-            $mess = "Please enter username";
+            $messa = "Please enter username";
         } else if(empty($password)) {
-            $mess = "Please enter password";
+            $messa = "Please enter password";
         } else if(empty($firstname)) {
-            $mess = "Please enter firstname";
+            $messa = "Please enter firstname";
         } else if(empty($lastname)) {
-            $mess = "Please enter lastname";
+            $messa = "Please enter lastname";
         } else if(empty($email)) {
-            $mess = "Please enter email";
+            $messa = "Please enter email";
         } else if(empty($university)) {
-            $mess = "Please enter university";
+            $messa = "Please enter university";
         } else { 
         
         $query = "INSERT INTO users (username, password, firstname, lastname, email, university) VALUES ('{$username}', '{$password}', '{$firstname}', '{$lastname}', '{$email}', '{$university}')";
@@ -63,33 +63,32 @@
 
 <html>
 
-  <link rel="stylesheet" type="text/css" href="css/signup.css"
-
-<body class="news">
-	
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    
     <?php if(isset($_SESSION["user"])) { ?>
     <div id="main2">
     <p>
         <center><h4>Welcome!</h4> Now that you are logged in you are able to submit rooms. <br> Head on over to the Search page, here you will can enter different infromation about the vacant room via the submission form.</center>
     </p>
     </div>
-    <?php } ?>	
-  
+    <?php } ?>
+	
+        <?php include_once("loginform.php"); ?> 
+
  	
-    <div id="para2">
+    <div id="main2">
     <p>
-        
-        <?php if(isset($mess)) { 
-         echo $mess; } ?>
-         
-      <center>Sign Up</center>
+        <center>Sign Up</center>
         <br>
         <center><h4>Once you have created an account you will be able to submit rooms to the site, allowing others to see the vacancy.</h4></center>
         <br>
         
+        <?php if(isset($mess)) { 
+         echo $mess; } ?>
+        
       <?php include_once("../includes/templates/signupform.php"); ?>
        
     </p>
-    </div>   
-             
+    </div>  
+                 
 <?php include_once("../includes/templates/footer.php"); ?> 
