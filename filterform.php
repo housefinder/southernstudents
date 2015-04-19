27dae57c27ea4d3c4d9512911507fd9f943e64da
Luke <lukepelling1@gmail.com>
2015-04-19 21:32:07 +0100

    Create filterform.php

---
+++ filterform.php
@@ -0,0 +1,41 @@
+<div id="main2">          
+<p>
+<link rel="stylesheet" type="text/css" href="css/formstyles.css">
+
+<center>Search By University:</center><br>
+    
+<form action="search.php" method="post" name="university">
+    <fieldset class="enter-info">
+
+            <label>
+            University
+	<select name="university">
+    <option value=""> - Select University - </option>
+    <option value="Arts University Bournemouth">Arts University Bournemouth</option>
+    <option value="University of Bath">University of Bath</option>
+    <option value="Bath Spa University">Bath Spa University</option>               
+    <option value="Bournemouth University">Bournemouth University</option>
+    <option value="University of Brighton">University of Brighton</option>         
+    <option value="University of Bristol">University of Bristol</option>
+    <option value="Exeter University">Exeter University</option>
+    <option value="University of Kent">University of Kent</option>
+    <option value="Oxford Brookes University">Oxford Brookes University</option>
+    <option value="Oxford University">Oxford University</option>
+    <option value="Plymouth University">Plymouth University</option>       
+    <option value="Portsmouth University">Portsmouth University</option>             
+    <option value="Reading University">Reading University</option>
+    <option value="Royal Halloway University of London">Royal Holloway University Of London</option>
+    <option value="University of Sussex">University of Sussex</option>
+    <option value="Southampton University">Southampton University</option>                  
+    <option value="U.W.E Bristol">U.W.E Bristol</option>            
+	</select>
+            </label>
+        </fieldset>
+    
+  <fieldset class="enter-action">
+    <input class="btn" type="submit" name="university-post" value="Search">
+  </fieldset>
+    
+</form>
+</p>
+</div>
