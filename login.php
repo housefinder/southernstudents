c5fd38c78da5510f440d2064196e3bdd8516ba72
Luke <lukepelling1@gmail.com>
2015-04-19 21:39:32 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,20 +2,22 @@
     require_once("../includes/session.php");
     require_once("../includes/connect.php");
     require_once("../includes/functions.php");
+    $_SESSION["message"] = "";
 
-    if(isset($_POST["login"])) {
+
+    if(isset($_POST["username"])) {
         
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        $query = "SELECT * FROM properties_users WHERE username='{$username}' AND password='{$password}' LIMIT 1";
+        $query = "SELECT * FROM users WHERE username='{$username}' AND password='{$password}' LIMIT 1";
         $result = mysqli_query($connection, $query);
-
+        
+        
         if($user = mysqli_fetch_assoc($result)) {
-            $_SESSION["message"] = "Success, welcome back {$user["username"]}";
-            $_SESSION["username"] = $user["username"];
+            $_SESSION["user"] = $user["username"];
             $_SESSION["user_id"] = $user["id"];
-            $_SESSION["user_level"] = 5;
+            $_SESSION["message"] = "Success, welcome back {$user["username"]}";
 
         } else {
             $_SESSION["message"] = "Wrong username/password";
@@ -23,6 +25,11 @@
 
     }
 
-    redirectTo("search.php");
+    redirectTo("signup.php");
 
+?>
+<?php 
+    if (isset ($connection)) {
+        mysqli_close($connection);    
+    }
 ?>
