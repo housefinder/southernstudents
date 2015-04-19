3eb3f663968d77c48d23db2c9759463c87e39e97
Luke <lukepelling1@gmail.com>
2015-04-19 21:34:27 +0100

    Update signupform.php

--- signupform.php
+++ signupform.php
@@ -1,64 +1,54 @@
-<?php if(isset($_SESSION["user"])) { ?>
-
-<div id="para2">          
-<p>      
 <link rel="stylesheet" type="text/css" href="css/formstyles.css">
-                
-    <form action="search.php" method="post">
-        <fieldset class="enter-info">
-      
-    <label> 
-    Address <input type="text" name="address" value="" placeholder="Enter">
-    </label>
-    
-    <label>
-    Postcode <input type="text" name="postcode" value="" placeholder="Enter">    
-    </label>
-    
-    <label>
-    Description <input type="text" name="description" value="" placeholder="Enter">
-    </label>
-    
-    <label>
-    Bedrooms <input type="text" name="bedrooms" value="" placeholder="Enter">
-    </label>
 
-    <label>
-    Gender
-    <input type="radio" name="gender" value="male">Male <input type="radio" name="gender" value="female">Female
-    </label> 
-    
-    <label>
-    University
+<form action="signup.php" method="post">
+    <fieldset class="enter-info">
+      
+            <label>
+            Username
+<input type="text" name="username" placeholder="Enter">
+            </label>
+            <label>
+            Password
+<input type="password" name="password" placeholder="Enter">
+            </label>
+            <label>
+            First Name
+<input type="text" name="firstname" placeholder="Enter">
+            </label>
+            <label>
+            Last Name
+<input type="text" name="lastname" placeholder="Enter">
+            </label>
+            <label>
+            E-mail
+<input type="text" name="email" placeholder="Enter">
+            </label>
+            <label>
+            University
 	<select name="university">
-    <option value="AUB">Arts University Bournemouth</option>
-    <option value="Bath">University of Bath</option>
-    <option value="BathSpa">Bath Spa University</option>               
-    <option value="Bournemouth">Bournemouth University</option>
-    <option value="Brighton">University of Brighton</option>         
-    <option value="Bristol">University of Bristol</option>
-    <option value="Exeter">Exeter University</option>
-    <option value="Kent">University of Kent</option>
-    <option value="OxfordBrookes">Oxford Brookes University</option>
-    <option value="Oxford">Oxford University</option>
-    <option value="Plymouth">Plymouth University</option>       
-    <option value="Portsmouth">Portsmouth University</option>             
-    <option value="Reading">Reading University</option>
-    <option value="RoyalHalloway">Royal Holloway University Of London</option>
-    <option value="Sussex">University of Sussex</option>
-    <option value="Southampton">Southampton University</option>                  
-    <option value="BristolUWE">U.W.E Bristol</option>             
+<option value="Arts University Bournemouth">Arts University Bournemouth</option>
+    <option value="University of Bath">University of Bath</option>
+    <option value="Bath Spa University">Bath Spa University</option>               
+    <option value="Bournemouth University">Bournemouth University</option>
+    <option value="University of Brighton">University of Brighton</option>         
+    <option value="University of Bristol">University of Bristol</option>
+    <option value="Exeter University">Exeter University</option>
+    <option value="University of Kent">University of Kent</option>
+    <option value="Oxford Brookes University">Oxford Brookes University</option>
+    <option value="Oxford University">Oxford University</option>
+    <option value="Plymouth University">Plymouth University</option>       
+    <option value="Portsmouth University">Portsmouth University</option>             
+    <option value="Reading University">Reading University</option>
+    <option value="Royal Halloway University of London">Royal Holloway University Of London</option>
+    <option value="University of Sussex">University of Sussex</option>
+    <option value="Southampton University">Southampton University</option>                  
+    <option value="U.W.E Bristol">U.W.E Bristol</option>             
 	</select>
-    </label>
+            </label>
         </fieldset>
     
   <fieldset class="enter-action">
     <input class="btn" type="submit" name="submit" value="Sign Up">
   </fieldset>
     
-    </form> 
-    
-    </p>      
-</div>
-
-<?php } ?>
+</form>
