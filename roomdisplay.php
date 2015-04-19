a6162eabdf57bff9316dedb6f6fc009aad0f96fd
Luke <lukepelling1@gmail.com>
2015-04-19 21:34:00 +0100

    Update roomdisplay.php

--- roomdisplay.php
+++ roomdisplay.php
@@ -1,15 +1,13 @@
-<html>
-<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+<div class="roomdisplay">
+    
+    <img class="image" width="150" height="150" src="../public/images/defhouse.jpg">	
+    <h2><?php echo $row["university"]; ?></h2>
+    <h4><?php echo $row["description"]; ?></h4>
+	<h4>Postcode: <?php echo $row["postcode"]; ?></h4>
+    <h4>Bedrooms: <?php echo $row["bedrooms"]; ?></h4>
+	<h4>Rent: £<?php echo $row["rent"]; ?></h4>
+    <h4>Contact Information: <?php echo $row["contact"]; ?></h4>
+    <h4>Data Added: <?php echo $row["date_added"]; ?></h4>
+  
 
-<div id="roomdisplay">
-    
-    <p> <!--Title displayed here
-        <br> Description displayed here
-        <br> Postcode displayed here
-        <br> Bedrooms displayed here
-        <br> Tenants gender displayed here
-        <br> University displayed here-->
-    </p> 
-    
 </div>
-</html>
