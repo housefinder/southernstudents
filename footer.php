2e48998109007928e3c18c4d5e55b368e0734204
Luke <lukepelling1@gmail.com>
2015-04-19 21:32:48 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <div id="footer">
     Copyright
+    <button type="button" class="ftb"><a href="meettheteam.php">Meet The Team</a></button>
 </div>  
 
         <script src="js/scripts.js"></script>
